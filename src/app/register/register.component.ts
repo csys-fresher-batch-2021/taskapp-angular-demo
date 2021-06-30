41d4ffb90b295ef2fdd2780a61aa2d556fa834a5
Naresh Kumar H <nareshkumarh@live.com>
2021-06-30 15:21:01 +0530

    Updated registration form validations

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -17,7 +17,7 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
     this.registerForm = this.formBuilder.group({
       name: new FormControl('',[Validators.required, Validators.minLength(3)]),
-      email: new FormControl('',[Validators.required]),
+      email: new FormControl('',[Validators.required, Validators.email]),
       password: new FormControl('',[Validators.required,  Validators.minLength(8)])
     })
   }
